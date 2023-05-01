5aa091a572ee906f54571adee4f68f38310ba387
lsklemm <73190190+lsklemm@users.noreply.github.com>
2023-05-01 10:18:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,46 +4,67 @@
 # 23.03.2023
 
 #((A\/B)/\((!B)\/C\/(!D))/\(D\/(!E)))
+#((A\/B\/(!C))(A\/C)(B\/(!C)))
 #((!A)/\(B\/C))
 #(A/\B)
 
 #(!(B\/C))
 #((A/\B)\/C)
-#((A/\(B\/(D/\E))))
+#((A/\B)\/C)
 
-def formula_validation(expr):
-    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    for symbol in expr:
-        if symbol not in '\/!()~->' and symbol not in alphabet and symbol not in '01':
+def correct_operator(expr):
+    new_expr = expr.replace('\/','v').replace('/\\', '^')
+    for symbol in new_expr:
+        if symbol in '\/':
             return False
-
-    if len(expr) == 1 and expr not in alphabet and expr != '1' and expr != '0':
-        return False
-    for index in range(len(expr)):
-        if expr[index] == ')' and index + 1 != len(expr):
-            if expr[index + 1] == '(':
-                return False
-        elif expr[index] in alphabet:
-            if expr[index + 1] in '01':
-                return False
-
     return True
 
+#Проверка на корректность введенных данных
+def formula_validation(expr):
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-def symbol_is_not_alone_in_brackets(formula: str) -> bool:
-    """
-    Проевряет, что атомарная формула указана без скобок
-    """
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    if len(formula) > 1:
+    if len(expr) == 1:
+        if expr not in alphabet:
+            return False
+        else:
+            return True
+    else:
+        if expr[0] != '(':
+            return False
+        if '(' not in expr and ')' not in expr:
+            return False
+        for symbol in expr:
+            if symbol not in '\/!()~->' and symbol not in alphabet and symbol not in '01':
+                return False
+
+        for index in range(len(expr)):
+            if expr[index] == ')' and index + 1 != len(expr):
+                if expr[index + 1] == '(':
+                    return False
+            elif expr[index] in alphabet:
+                if expr[index + 1] in '01':
+                    return False
+
+        return True
+
+
+#Проверка, что символ находится в скобках не один
+def symbol_is_not_alone_in_brackets(expr: str) -> bool:
+
+    symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ01'
+    if len(expr) > 1:
         try:
-            for symbol_index in range(0, len(formula)):
-                # if formula[symbol_index] == '(' and formula_validation(formula[symbol_index + 1]) and formula[symbol_index + 2] == ')':
+            for symbol_index in range(0, len(expr)):
+                if expr[symbol_index] == '(' and expr[symbol_index + 1] in symbols and expr[symbol_index + 2] == ')':
+                    return False
+                # if formula_validation(formula[symbol_index + 1]):
                 #     return False
-                if formula[symbol_index] in alphabet.upper() and formula[symbol_index+1] in alphabet.upper():
+                elif expr[symbol_index] in symbols and expr[symbol_index + 1] in symbols:
                     return False
-                elif formula[symbol_index] == '!' and (formula[symbol_index + 1] == ')' or formula[symbol_index - 1] != '('):
+                elif expr[symbol_index] == '!' and (expr[symbol_index + 1] == ')' or expr[symbol_index - 1] != '('):
                     return False
+                # elif formula[symbol_index] == '~' and (formula[symbol_index + 1] == ')' or formula[symbol_index - 1] != '('):
+                #     return False
 
             return True
         except:
@@ -52,6 +73,7 @@ def symbol_is_not_alone_in_brackets(formula: str) -> bool:
         return True
 
 
+#Проверка на лишние двойные скобки
 def double_brackets(expr):
     count = 0
     for index in range(len(expr)):
@@ -69,10 +91,10 @@ def double_brackets(expr):
                         if index2 + 1 == len(expr):
                             return True
                         else:
-                            if  expr[index2 + 1] == ')':
+                            if expr[index2 + 1] == ')':
                                 break
     # print(count)
-    if count == 1:
+    if count == 2:
         return False
     else:
         return True
@@ -82,6 +104,10 @@ def double_brackets(expr):
 
 
 def is_cnf(expr):
+    # Проверяем, что количество открывающих скобок равно количеству закрывающих скобок
+    if expr.count('(') != expr.count(')'):
+        return False
+
     # Разбиваем выражение на конъюнкции
     clauses = expr.split('/\\')
 
@@ -99,24 +125,47 @@ def is_cnf(expr):
                 return False
 
             # Проверяем, что литерал содержит только переменную или ее отрицание
-            # for s in literal:
-            #     if s.isalpha():
-            #         print(s)
-
-            if len(literal.strip()) > 1 and (literal[0] != '!' or not literal[1:].isalpha()):
+            new_literal = ''
+            for s in literal:
+                if s != '(' and s != ')':
+                    new_literal += s
+            if len(new_literal.strip()) > 1 and (new_literal[0] != '!' or not new_literal[1:].isalpha()):
+                print(new_literal)
+                print('here')
                 return False
-    # Если все условия выполнены, то выражение является КНФ
-    return True
+        # Если все условия выполнены, то выражение является КНФ
+        return True
 
 
 expr = input("Введите логическое выражение: ")
-print(formula_validation(expr))
-print(double_brackets(expr))
-print(symbol_is_not_alone_in_brackets(expr))
-if formula_validation(expr) and symbol_is_not_alone_in_brackets(expr) and double_brackets(expr):
-    if is_cnf(expr):
-        print("Выражение является КНФ")
+# print(formula_validation(expr))
+# print(double_brackets(expr))
+# print(symbol_is_not_alone_in_brackets(expr))
+
+if formula_validation(expr):
+    if symbol_is_not_alone_in_brackets(expr):
+        if double_brackets(expr):
+            if correct_operator(expr):
+                if is_cnf(expr):
+                    print("Выражение является КНФ")
+                else:
+                    print("Выражение не является КНФ")
+            else:
+                print('Формула введена некорректно(1)')
+        else:
+            print('Формула введена некорректно(2)')
     else:
-        print("Выражение не является КНФ")
+        print('Формула введена некорректно(3)')
 else:
-    print('Формула введена некорректно')
+    print('Формула введена некорректно(4)')
+
+# (((A\/B)\/C)/\((!A)\/((B\/C)/\(A\/((!B)\/C))))
+# ((((A\/B)\/C)/\((A\/(!B))\/C))/\(((!A)\/B)\/C))
+#((A/\B/\C))
+#((A\/(!B))/\())
+
+#1,0
+#(A/\/B)
+#A(\/A)
+#\/A
+
